b27eeea0a8ac4419efd5a6e513cfe5c459ccdcbf
Percy Ivan Quispe Escalante <pquispe@soaint.com>
2024-06-03 22:16:17 -0500

    fuente base

---
+++ GestionConocimiento/GestionConocimiento/src/main/java/com/integrador/GestionConocimiento/GestionConocimientoApplication.java
@@ -0,0 +1,13 @@
+package com.integrador.GestionConocimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionConocimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionConocimientoApplication.class, args);
+	}
+
+}
---
+++ GestionConocimiento/GestionConocimiento/src/test/java/com/integrador/GestionConocimiento/GestionConocimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integrador.GestionConocimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionConocimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
